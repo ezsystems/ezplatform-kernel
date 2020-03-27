03573d5c29944f4b598af8e68a8031e42255863e
barw4 <bartek.rofl@gmail.com>
2020-03-27 11:51:07 +0100

    EZP-31383: Implemented Role copying API

--- eZ/Publish/Core/Persistence/Legacy/User/Role/Gateway/DoctrineDatabase.php
+++ eZ/Publish/Core/Persistence/Legacy/User/Role/Gateway/DoctrineDatabase.php
@@ -86,6 +86,36 @@ final class DoctrineDatabase extends Gateway
         return $role;
     }
 
+    public function copyRole(Role $role): Role
+    {
+        $roleOriginalId = $role->status;
+
+        $query = $this->connection->createQueryBuilder();
+        $query
+            ->insert(self::ROLE_TABLE)
+            ->values(
+                [
+                    'is_new' => $query->createPositionalParameter(0, ParameterType::INTEGER),
+                    'name' => $query->createPositionalParameter(
+                        $role->identifier,
+                        ParameterType::STRING
+                    ),
+                    'value' => $query->createPositionalParameter(0, ParameterType::INTEGER),
+                    // BC: "version" stores originalId when creating a draft from an existing role
+                    'version' => $query->createPositionalParameter(
+                        $roleOriginalId,
+                        ParameterType::STRING
+                    ),
+                ]
+            );
+        $query->execute();
+
+        $role->id = (int)$this->connection->lastInsertId(self::ROLE_SEQ);
+        $role->originalId = $roleOriginalId;
+
+        return $role;
+    }
+
     private function getLoadRoleQueryBuilder(): QueryBuilder
     {
         $query = $this->connection->createQueryBuilder();
