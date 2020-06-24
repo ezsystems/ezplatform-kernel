9b1bd8ec42fcdfd253b42671caafd0fab67efbe4
barw4 <bartek.rofl@gmail.com>
2020-06-24 10:19:38 +0200

    EZP-31595: Parametrized embedded Content's URL generation

--- eZ/Publish/Core/MVC/Symfony/View/Builder/ContentViewBuilder.php
+++ eZ/Publish/Core/MVC/Symfony/View/Builder/ContentViewBuilder.php
@@ -154,6 +154,15 @@ class ContentViewBuilder implements ViewBuilder
             $view->setLocation($location);
         }
 
+        if ($view->isEmbed()) {
+            if (
+                $this->permissionResolver->canUser('content', 'view_embed', $content->contentInfo)
+                && !$this->permissionResolver->canUser('content', 'read', $content->contentInfo)
+            ) {
+                $parameters['params']['objectParameters'] = ['doNotGenerateEmbedUrl' => true];
+            }
+        }
+
         $this->viewParametersInjector->injectViewParameters($view, $parameters);
         $this->viewConfigurator->configure($view);
 
