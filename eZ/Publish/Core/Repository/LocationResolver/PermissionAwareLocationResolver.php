a391bcb8accfea196e52a54ebab2e05e1808444b
barw4 <bartek.rofl@gmail.com>
2020-07-29 16:13:04 +0200

    EZP-31584: CS

--- eZ/Publish/Core/Repository/LocationResolver/PermissionAwareLocationResolver.php
+++ eZ/Publish/Core/Repository/LocationResolver/PermissionAwareLocationResolver.php
@@ -44,7 +44,7 @@ final class PermissionAwareLocationResolver implements LocationResolver
         } catch (NotFoundException | UnauthorizedException $e) {
             // try different locations if main location is not accessible for the user
             $locations = $this->locationService->loadLocations($contentInfo);
-            if (empty($locations) OR null === $contentInfo->mainLocationId) {
+            if (empty($locations) or null === $contentInfo->mainLocationId) {
                 throw $e;
             }
 
