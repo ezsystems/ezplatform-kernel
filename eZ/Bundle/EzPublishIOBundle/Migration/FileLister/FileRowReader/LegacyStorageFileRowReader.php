ff086c72a022493f1d830363239be04efb73866f
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2020-09-29 13:51:00 -0400

    Update eZ/Bundle/EzPublishIOBundle/Migration/FileLister/FileRowReader/LegacyStorageFileRowReader.php
    Co-authored-by: Andrew Longosz <alongosz@users.noreply.github.com>

--- eZ/Bundle/EzPublishIOBundle/Migration/FileLister/FileRowReader/LegacyStorageFileRowReader.php
+++ eZ/Bundle/EzPublishIOBundle/Migration/FileLister/FileRowReader/LegacyStorageFileRowReader.php
@@ -42,7 +42,7 @@ abstract class LegacyStorageFileRowReader implements FileRowReaderInterface
     {
         $row = $this->statement->fetch();
 
-        return is_array($row) ? $this->prependMimeToPath($row['filename'], $row['mime_type']) : null;
+        return false !== $row ? $this->prependMimeToPath($row['filename'], $row['mime_type']) : null;
     }
 
     final public function getCount()
