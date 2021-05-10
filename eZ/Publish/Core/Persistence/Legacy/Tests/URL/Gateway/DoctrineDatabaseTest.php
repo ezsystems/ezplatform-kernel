b1ca2cd36498e885c22704bcf2fb923fe4674ea8
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2021-05-10 09:04:42 +0200

    Fixed sonarcloud-detected issues

--- eZ/Publish/Core/Persistence/Legacy/Tests/URL/Gateway/DoctrineDatabaseTest.php
+++ eZ/Publish/Core/Persistence/Legacy/Tests/URL/Gateway/DoctrineDatabaseTest.php
@@ -25,45 +25,27 @@ class DoctrineDatabaseTest extends TestCase
      */
     protected $gateway;
 
-    protected $fixtureData = [
-        [
-            'id' => 23,
-            'created' => 1448832197,
-            'is_valid' => 1,
-            'last_checked' => 0,
-            'modified' => 1448832197,
-            'original_url_md5' => 'f76e41d421b2a72232264943026a6ee5',
-            'url' => 'https://doc.ez.no/display/USER/',
-        ],
-        [
-            'id' => 24,
-            'created' => 1448832277,
-            'is_valid' => 1,
-            'last_checked' => 0,
-            'modified' => 1505717756,
-            'original_url_md5' => 'a00ab36edb35bb641cc027eb27410934',
-            'url' => 'https://doc.ezplatform.com/en/latest/',
-        ],
-        [
-            'id' => 25,
-            'created' => 1448832412,
-            'is_valid' => 1,
-            'last_checked' => 0,
-            'modified' => 1505717756,
-            'original_url_md5' => '03c4188f5fdcb679192e25a7dad09c2d',
-            'url' => 'https://doc.ezplatform.com/en/latest/tutorials/platform_beginner/building_a_bicycle_route_tracker_in_ez_platform/',
-        ],
-    ];
+    /**
+     * @var array[]
+     */
+    protected $fixtureData;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $fixtureLocation = __DIR__ . '/_fixtures/urls.php';
+        $this->fixtureData = (require $fixtureLocation)['ezurl'];
+        $this->insertDatabaseFixture($fixtureLocation);
+        $this->initGateway();
+    }
 
     /**
      * @covers \eZ\Publish\Core\Persistence\Legacy\URL\Gateway\DoctrineDatabase::loadUrlData
      */
     public function testLoadUrlData()
     {
-        $this->insertDatabaseFixture(__DIR__ . '/_fixtures/urls.php');
-        $gateway = $this->getGateway();
-
-        $row = $gateway->loadUrlData(23);
+        $row = $this->gateway->loadUrlData(23);
 
         self::assertEquals(
             $this->fixtureData[0],
@@ -76,10 +58,7 @@ class DoctrineDatabaseTest extends TestCase
      */
     public function testLoadUrlDataByUrl()
     {
-        $this->insertDatabaseFixture(__DIR__ . '/_fixtures/urls.php');
-        $gateway = $this->getGateway();
-
-        $rows = $gateway->loadUrlDataByUrl('https://doc.ez.no/display/USER/');
+        $rows = $this->gateway->loadUrlDataByUrl('https://doc.ez.no/display/USER/');
 
         self::assertEquals(
             $this->fixtureData[0],
@@ -92,11 +71,8 @@ class DoctrineDatabaseTest extends TestCase
      */
     public function testFind()
     {
-        $this->insertDatabaseFixture(__DIR__ . '/_fixtures/urls.php');
-        $gateway = $this->getGateway();
-
         $criterion = new \eZ\Publish\API\Repository\Values\URL\Query\Criterion\MatchAll();
-        $results = $gateway->find($criterion, 0, 10);
+        $results = $this->gateway->find($criterion, 0, 10);
 
         self::assertEquals(
             [
@@ -112,11 +88,8 @@ class DoctrineDatabaseTest extends TestCase
      */
     public function testFindWithDisabledCounting()
     {
-        $this->insertDatabaseFixture(__DIR__ . '/_fixtures/urls.php');
-        $gateway = $this->getGateway();
-
         $criterion = new \eZ\Publish\API\Repository\Values\URL\Query\Criterion\MatchAll();
-        $results = $gateway->find($criterion, 0, 10, [], false);
+        $results = $this->gateway->find($criterion, 0, 10, [], false);
 
         self::assertEquals(
             [
@@ -130,7 +103,7 @@ class DoctrineDatabaseTest extends TestCase
     /**
      * Return the DoctrineDatabase gateway to test.
      */
-    protected function getGateway(): DoctrineDatabase
+    protected function initGateway(): DoctrineDatabase
     {
         if (!isset($this->gateway)) {
             $criteriaConverter = new CriteriaConverter([new MatchAll()]);
