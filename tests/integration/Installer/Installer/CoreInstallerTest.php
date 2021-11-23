e6c4c75eebda260ffc53352fce818d37d20c968f
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2021-11-23 14:18:20 +0100

    IBX-1489: Provided integration test for Installers for PHP8

---
+++ tests/integration/Installer/Installer/CoreInstallerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\Tests\Integration\Installer\Installer;
+
+use EzSystems\PlatformInstallerBundle\Installer\CoreInstaller;
+use Ibexa\Tests\Integration\Installer\TestCase;
+use Symfony\Component\Console\Output\NullOutput;
+
+final class CoreInstallerTest extends TestCase
+{
+    /** @var \EzSystems\PlatformInstallerBundle\Installer\CoreInstaller */
+    private $installer;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $this->installer = self::getServiceByClassName(CoreInstaller::class);
+    }
+
+    public function testImportSchema(): void
+    {
+        $this->installer->setOutput(new NullOutput());
+        $this->installer->importSchema();
+    }
+}
