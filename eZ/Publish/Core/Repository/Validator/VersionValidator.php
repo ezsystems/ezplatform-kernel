11bc3af7d27e07fba280f09a3162da198d814a42
matx132 <mateusz@mateuszdebinski.pl>
2021-12-23 11:57:06 +0100

    Added the second argument to getField, to prevent get field value only for the main language

--- eZ/Publish/Core/Repository/Validator/VersionValidator.php
+++ eZ/Publish/Core/Repository/Validator/VersionValidator.php
@@ -68,7 +68,7 @@ final class VersionValidator implements ContentValidator
             );
 
             foreach ($languageCodes as $languageCode) {
-                $fieldValue = $content->getField($fieldDefinition->identifier)->value ?? $fieldDefinition->defaultValue;
+                $fieldValue = $content->getField($fieldDefinition->identifier, $languageCode)->value ?? $fieldDefinition->defaultValue;
                 $fieldValue = $fieldType->acceptValue($fieldValue);
 
                 if ($fieldType->isEmptyValue($fieldValue)) {
