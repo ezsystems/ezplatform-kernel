ccece31e27af7cc140aed72799d75d917143c005
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2021-12-29 12:02:58 +0100

    IBX-1809: Allowed override of test Kernel via environment

--- src/contracts/Test/IbexaKernelTestCase.php
+++ src/contracts/Test/IbexaKernelTestCase.php
@@ -41,7 +41,7 @@ abstract class IbexaKernelTestCase extends KernelTestCase
         try {
             return parent::getKernelClass();
         } catch (LogicException $e) {
-            trigger_error(sprintf(
+            @trigger_error(sprintf(
                 'You should set the KERNEL_CLASS environment variable to the fully-qualified class name of your '
                 . 'Kernel in phpunit.xml / phpunit.xml.dist or override the "%1$s::createKernel()" or '
                 . '"%1$s::getKernelClass()" method.',
