3524a95b80ae977a4526776b408a891d219dc874
Dawid Parafiński <dawid.parafinski@ez.no>
2022-09-22 14:19:49 +0200

    IBX-3823: Fixed appending limitations, when there are no initial values (#335)

--- eZ/Bundle/EzPublishCoreBundle/Tests/DependencyInjection/Security/PolicyProvider/PoliciesConfigBuilderTest.php
+++ eZ/Bundle/EzPublishCoreBundle/Tests/DependencyInjection/Security/PolicyProvider/PoliciesConfigBuilderTest.php
@@ -14,20 +14,46 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PoliciesConfigBuilderTest extends TestCase
 {
-    public function testAddConfig()
+    /**
+     * @dataProvider policiesConfigProvider
+     */
+    public function testAddConfig(array $configOne, array $configTwo, array $expectedConfig): void
     {
         $containerBuilder = new ContainerBuilder();
         $configBuilder = new PoliciesConfigBuilder($containerBuilder);
-        $config1 = ['foo' => ['bar' => null]];
-        $config2 = ['some' => ['thing' => ['limitation']]];
-        $expected = [
-            'foo' => ['bar' => []],
-            'some' => ['thing' => ['limitation' => true]],
-        ];
-        $configBuilder->addConfig($config1);
-        $configBuilder->addConfig($config2);
 
-        self::assertSame($expected, $containerBuilder->getParameter('ezpublish.api.role.policy_map'));
+        $configBuilder->addConfig($configOne);
+        $configBuilder->addConfig($configTwo);
+
+        self::assertSame($expectedConfig, $containerBuilder->getParameter('ezpublish.api.role.policy_map'));
+    }
+
+    public function policiesConfigProvider(): array
+    {
+        return [
+            'add' => [
+                ['foo' => ['bar' => null]],
+                ['some' => ['thing' => ['limitation']]],
+                [
+                    'foo' => ['bar' => []],
+                    'some' => ['thing' => ['limitation' => true]],
+                ],
+            ],
+            'append' => [
+                ['foo' => ['bar' => ['limitation']]],
+                ['foo' => ['bar' => ['new_limitation']]],
+                [
+                    'foo' => ['bar' => ['limitation' => true, 'new_limitation' => true]],
+                ],
+            ],
+            'append_to_empty' => [
+                ['foo' => ['bar' => null]],
+                ['foo' => ['bar' => ['new_limitation']]],
+                [
+                    'foo' => ['bar' => ['new_limitation' => true]],
+                ],
+            ],
+        ];
     }
 
     public function testAddResource()
