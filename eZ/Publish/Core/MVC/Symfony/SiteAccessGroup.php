de2bfe2ab4b0e25aad17817dae4ecf4ea288d767
Dawid Parafiński <dawid.parafinski@ez.no>
2022-12-14 14:57:36 +0100

    IBX-4581: Added missing groups setup when siteaccess is de/serialized

--- eZ/Publish/Core/MVC/Symfony/SiteAccessGroup.php
+++ eZ/Publish/Core/MVC/Symfony/SiteAccessGroup.php
@@ -8,7 +8,9 @@ declare(strict_types=1);
 
 namespace eZ\Publish\Core\MVC\Symfony;
 
-final class SiteAccessGroup
+use JsonSerializable;
+
+final class SiteAccessGroup implements JsonSerializable
 {
     /** @var string */
     private $name;
@@ -26,5 +28,12 @@ final class SiteAccessGroup
     public function __toString()
     {
         return $this->name;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'name' => $this->name,
+        ];
     }
 }
