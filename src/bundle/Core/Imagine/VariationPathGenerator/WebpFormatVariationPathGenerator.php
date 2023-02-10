d53b18fb12f3f71895c906e6b189f37732b2e9a6
Maciej Kobus <maciej.kobus@ez.no>
2023-02-10 09:01:44 +0100

    fixup! IBX-4647: Added type hints

--- src/bundle/Core/Imagine/VariationPathGenerator/WebpFormatVariationPathGenerator.php
+++ src/bundle/Core/Imagine/VariationPathGenerator/WebpFormatVariationPathGenerator.php
@@ -30,7 +30,7 @@ final class WebpFormatVariationPathGenerator implements VariationPathGenerator
         $this->filterConfiguration = $filterConfiguration;
     }
 
-    public function getVariationPath(string $originalPath, string $filter): string
+    public function getVariationPath($originalPath, $filter): string
     {
         $variationPath = $this->innerVariationPathGenerator->getVariationPath($originalPath, $filter);
         $filterConfig = $this->filterConfiguration->get($filter);
