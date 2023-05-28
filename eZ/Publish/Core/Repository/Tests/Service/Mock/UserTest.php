17f68993b21ae58a6e02a65a9b3e2f3f97779b03
Andrew Longosz <alongosz@users.noreply.github.com>
2023-05-28 17:33:20 +0200

    fixup! fixup! [Tests] Aligned UserService unit tests

--- eZ/Publish/Core/Repository/Tests/Service/Mock/UserTest.php
+++ eZ/Publish/Core/Repository/Tests/Service/Mock/UserTest.php
@@ -6,10 +6,11 @@
  */
 namespace eZ\Publish\Core\Repository\Tests\Service\Mock;
 
+use Exception;
 use eZ\Publish\API\Repository\ContentService as APIContentService;
 use eZ\Publish\API\Repository\PasswordHashService;
+use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\API\Repository\UserService as APIUserService;
-use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use eZ\Publish\API\Repository\Values\Content\ContentInfo as APIContentInfo;
 use eZ\Publish\API\Repository\Values\Content\VersionInfo as APIVersionInfo;
 use eZ\Publish\API\Repository\Values\User\User;
@@ -17,8 +18,8 @@ use eZ\Publish\API\Repository\Values\User\User as APIUser;
 use eZ\Publish\Core\Repository\Tests\Service\Mock\Base as BaseServiceMockTest;
 use eZ\Publish\Core\Repository\User\PasswordValidatorInterface;
 use eZ\Publish\Core\Repository\UserService;
+use eZ\Publish\SPI\Persistence\User\Handler as PersistenceUserHandler;
 use eZ\Publish\SPI\Persistence\User\RoleAssignment;
-use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * @covers \eZ\Publish\Core\Repository\UserService
@@ -40,17 +41,10 @@ class UserTest extends BaseServiceMockTest
 
         $user = $this->createMock(APIUser::class);
         $contentInfo = $this->createMock(APIContentInfo::class);
-
-        $this->configureUserContentMocks($user, $contentInfo, $userService);
-
-        $this->mockRoleAssignmentRemoval($userHandler, self::MOCKED_USER_ID);
-
-        $repository->expects(self::once())->method('beginTransaction');
+        $this->mockDeleteUserFlow($repository, $userService, $contentService, $user, $contentInfo, $userHandler);
 
         $contentService->expects(self::once())->method('deleteContent')->with($contentInfo);
-        $repository->expects(self::once())->method('getContentService')->willReturn($contentService);
         $userHandler->expects(self::once())->method('delete')->with(self::MOCKED_USER_ID);
-
         $repository->expects(self::once())->method('commit');
 
         $userService->deleteUser($user);
@@ -66,41 +60,23 @@ class UserTest extends BaseServiceMockTest
         $repository = $this->getRepositoryMock();
         $userService = $this->getPartlyMockedUserService(['loadUser']);
         $contentService = $this->createMock(APIContentService::class);
-        /* @var \PHPUnit\Framework\MockObject\MockObject $userHandler */
+        /* @var \eZ\Publish\SPI\Persistence\User\Handler&\PHPUnit\Framework\MockObject\MockObject $userHandler */
         $userHandler = $this->getPersistenceMock()->userHandler();
 
         $user = $this->createMock(APIUser::class);
         $contentInfo = $this->createMock(APIContentInfo::class);
+        $this->mockDeleteUserFlow($repository, $userService, $contentService, $user, $contentInfo, $userHandler);
 
-        $this->configureUserContentMocks($user, $contentInfo, $userService);
-
-        $this->mockRoleAssignmentRemoval($userHandler, self::MOCKED_USER_ID);
-
-        $repository->expects(self::once())->method('beginTransaction');
-
-        $exception = new \Exception();
+        $exception = new Exception();
         $contentService->expects(self::once())
             ->method('deleteContent')
             ->with($contentInfo)
             ->willThrowException($exception);
 
-        $repository->expects(self::once())->method('getContentService')->willReturn($contentService);
-
         $repository->expects(self::once())->method('rollback');
 
         $this->expectExceptionObject($exception);
         $userService->deleteUser($user);
-    }
-
-    private function mockRoleAssignmentRemoval(MockObject $userHandler, int $userId): void
-    {
-        $userHandler
-            ->expects(self::once())
-            ->method('loadRoleAssignmentsByGroupId')
-            ->with($userId)
-            ->willReturn([new RoleAssignment(['id' => 1])]);
-
-        $userHandler->method('removeRoleAssignment')->with(1);
     }
 
     /**
@@ -130,12 +106,21 @@ class UserTest extends BaseServiceMockTest
     }
 
     /**
+     * @param \eZ\Publish\API\Repository\Repository&\PHPUnit\Framework\MockObject\MockObject $repository
+     * @param \eZ\Publish\API\Repository\UserService&\PHPUnit\Framework\MockObject\MockObject $userService
+     * @param \eZ\Publish\API\Repository\ContentService&\PHPUnit\Framework\MockObject\MockObject $contentService
      * @param \eZ\Publish\API\Repository\Values\User\User&\PHPUnit\Framework\MockObject\MockObject $user
      * @param \eZ\Publish\API\Repository\Values\Content\ContentInfo&\PHPUnit\Framework\MockObject\MockObject $contentInfo
-     * @param \eZ\Publish\API\Repository\UserService&\PHPUnit\Framework\MockObject\MockObject $userService
+     * @param \eZ\Publish\SPI\Persistence\User\Handler&\PHPUnit\Framework\MockObject\MockObject $userHandler
      */
-    private function configureUserContentMocks(User $user, ContentInfo $contentInfo, APIUserService $userService): void
-    {
+    private function mockDeleteUserFlow(
+        Repository $repository,
+        APIUserService $userService,
+        APIContentService $contentService,
+        User $user,
+        APIContentInfo $contentInfo,
+        PersistenceUserHandler $userHandler
+    ): void {
         $loadedUser = $this->createMock(APIUser::class);
         $versionInfo = $this->createMock(APIVersionInfo::class);
 
@@ -143,6 +128,18 @@ class UserTest extends BaseServiceMockTest
         $versionInfo->method('getContentInfo')->willReturn($contentInfo);
         $loadedUser->method('getVersionInfo')->willReturn($versionInfo);
         $loadedUser->method('__get')->with('id')->willReturn(self::MOCKED_USER_ID);
+
         $userService->method('loadUser')->with(self::MOCKED_USER_ID)->willReturn($loadedUser);
+
+        $userHandler
+            ->expects(self::once())
+            ->method('loadRoleAssignmentsByGroupId')
+            ->with(self::MOCKED_USER_ID)
+            ->willReturn([new RoleAssignment(['id' => 1])]);
+
+        $userHandler->method('removeRoleAssignment')->with(1);
+
+        $repository->expects(self::once())->method('beginTransaction');
+        $repository->expects(self::once())->method('getContentService')->willReturn($contentService);
     }
 }
