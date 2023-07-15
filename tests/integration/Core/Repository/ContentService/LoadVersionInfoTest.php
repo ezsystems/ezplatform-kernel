85bedd90f9f63c06eb899b958a1c9c2498cb8cd8
Andrew Longosz <alongosz@users.noreply.github.com>
2023-07-15 17:05:12 +0200

    [Tests] Refactored feature integration coverage to use IbexaTestKernel

---
+++ tests/integration/Core/Repository/ContentService/LoadVersionInfoTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Integration\Core\Repository\ContentService;
+
+use Ibexa\Tests\Integration\Core\RepositoryTestCase;
+
+/**
+ * @covers \eZ\Publish\API\Repository\ContentService
+ */
+final class LoadVersionInfoTest extends RepositoryTestCase
+{
+    /**
+     * @throws \eZ\Publish\API\Repository\Exceptions\Exception
+     */
+    public function testLoadVersionInfoListByContentInfo(): void
+    {
+        $contentService = self::getContentService();
+
+        $folder1 = $this->createFolder(['eng-GB' => 'Folder1'], 2);
+        $folder2 = $this->createFolder(['eng-GB' => 'Folder2'], 2);
+
+        $versionInfoList = $contentService->loadVersionInfoListByContentInfo(
+            [
+                $folder1->getVersionInfo()->getContentInfo(),
+                $folder2->getVersionInfo()->getContentInfo(),
+            ]
+        );
+
+        self::assertCount(2, $versionInfoList);
+
+        foreach ($versionInfoList as $versionInfo) {
+            $loadedVersionInfo = $contentService->loadVersionInfo(
+                $versionInfo->getContentInfo(),
+                $versionInfo->versionNo
+            );
+            self::assertEquals($loadedVersionInfo, $versionInfo);
+        }
+    }
+}
