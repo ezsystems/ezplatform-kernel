e72fd7549f761058fed8ba9340572d54fab844d6
Nattfarinn <nattfarinn@gmail.com>
2024-08-07 09:59:40 +0200

    fix: Replace null comparison with isset

--- eZ/Publish/Core/Persistence/Legacy/Content/FieldHandler.php
+++ eZ/Publish/Core/Persistence/Legacy/Content/FieldHandler.php
@@ -332,7 +332,7 @@ class FieldHandler
                 if (isset($updateFieldMap[$fieldDefinition->id][$languageCode])) {
                     $field = clone $updateFieldMap[$fieldDefinition->id][$languageCode];
                     $field->versionNo = $content->versionInfo->versionNo;
-                    if (null !== $field->id && array_key_exists($field->languageCode, $existingLanguageCodes)) {
+                    if (isset($field->id) && array_key_exists($field->languageCode, $existingLanguageCodes)) {
                         $this->updateField($field, $content);
                         $updatedFields[$fieldDefinition->id][$languageCode] = $field;
                     } else {
